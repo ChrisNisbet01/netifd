8c1ee7f4f44121e6127996e112fc0425f3c08c10
Chris Nisbet <cnisbet@digi.com>
2021-08-21 11:09:32 +1200

    Author: Li Zhen Date: 2021-04-01 14:41:41 Revision: fb8d81b06e73142a80708ffa31410a1a43c0c3c6 Comment: wifi: added wpa3 support for both client and access point modes
    Added wpa2/wpa3 mixed mode support
    Added wpa3-owe aka Enhanced Open (OWE) support
    Added wpa3-personal (SAE) support
    Added wpa3-enterprise support on TX64 and TX64-PR platforms
    
    Note that wpa3-enterprise is only supported on wave 2 wifi chipset
    
    DAL-4672 DAL-4673 DAL-4752 DAL-4754

--- scripts/netifd-wireless.sh
+++ scripts/netifd-wireless.sh
@@ -198,7 +198,7 @@ wireless_vif_parse_encryption() {
 	wpa_pairwise=CCMP
 	case "$encryption" in
 		*tkip+aes|*tkip+ccmp|*aes+tkip|*ccmp+tkip) wpa_pairwise="CCMP TKIP";;
-		*aes|*ccmp) wpa_pairwise="CCMP";;
+		*aes|*ccmp|*sae) wpa_pairwise="CCMP";;
 		*tkip) wpa_pairwise="TKIP";;
 	esac
 
@@ -211,7 +211,7 @@ wireless_vif_parse_encryption() {
 	# wpa2/tkip+aes     => WPA2 RADIUS, CCMP+TKIP
 
 	case "$encryption" in
-		wpa2*|*psk2*)
+		wpa2*|*psk2*|wpa3*|*sae*|*owe*)
 			wpa=2
 		;;
 		*mixed*)
@@ -227,8 +227,24 @@ wireless_vif_parse_encryption() {
 	esac
 
 	case "$encryption" in
-		*psk*)
+		*psk2sae)
+			auth_type=psk2sae
+		;;
+		*owe)
+			auth_type=owe
+		;;
+		*psk)
 			auth_type=psk
+		;;
+		*psk2)
+			auth_type=psk
+		;;
+		*sae*)
+			auth_type=sae
+		;;
+		*wpa3*)
+			auth_type=eap192
+			wpa_pairwise="GCMP-256"
 		;;
 		*wpa*|*8021x*)
 			auth_type=eap
